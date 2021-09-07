5cff048917822484043e47d3e55e9bf36e493ae8
Victor Alessander <alessander@protonmail.com>
2021-09-07 13:34:21 -0300

    spider setup

--- filmfish/spiders/fishfilmspider.py
+++ filmfish/spiders/fishfilmspider.py
@@ -3,7 +3,7 @@ from scrapy import Spider
 
 class FishFilmSpider(Spider):
     name = "fishfilm"
-    start_urls = []
+    start_urls = ["https://www.film-fish.com/"]
 
     def parse(self, response, **kwargs):
         pass
